bf6afdfef703a2e8ebee967628afcac77fa57f19
tamanugi <tamanugi@gmail.com>
2018-10-13 15:07:07 +0900

    とりあえずチャット部分を仮組み

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,23 +1,57 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  .message_board
+    .message(v-for="message in messages")
+      span {{message}}
+  .message_form_wrapper
+    input(type=text, v-model="input_message")
+    button(@click="shoutMessage") 送信
 </template>
 
 <script>
+import { Socket } from '../static/phoenix'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data: function () {
+    return {
+      channel: {},
+      messages: ['hoge', 'hoge2'],
+      input_message: ''
+    }
+  },
+  methods: {
+    shoutMessage: function () {
+      console.log(this.input_message)
+      if (this.input_message) {
+        this.channel.push('shout', {message: this.input_message})
+        this.input_message = ''
+      }
+    }
+  },
+  mounted: function () {
+    let socket = new Socket('ws://localhost:4000/socket')
+    socket.connect()
+    let channel = socket.channel('room:lobby', {})
+
+    channel.on('shout', payload => {
+      console.log(payload)
+      this.messages.push(payload.message)
+    })
+
+    channel.join()
+
+    this.channel = channel
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
 </style>
--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,3 +1,30 @@
 // for phoenix_html support, including form and button helpers
 // copy the following scripts into your javascript bundle:
 // * https://raw.githubusercontent.com/phoenixframework/phoenix_html/v2.10.0/priv/static/phoenix_html.js
+
+let channel = socket.channel('room:lobby', {}); // connect to chat "room"
+
+channel.on('shout', function (payload) { // listen to the 'shout' event
+  let li = document.createElement("li"); // creaet new list item DOM element
+  let name = payload.name || 'guest';    // get name from payload or set default
+  li.innerHTML = '<b>' + name + '</b>: ' + payload.message; // set li contents
+  ul.appendChild(li);                    // append to list
+});
+
+channel.join(); // join the channel.
+
+
+let ul = document.getElementById('msg-list');        // list of messages.
+let name = document.getElementById('name');          // name of message sender
+let msg = document.getElementById('msg');            // message input field
+
+// "listen" for the [Enter] keypress event to send a message:
+msg.addEventListener('keypress', function (event) {
+  if (event.keyCode == 13 && msg.value.length > 0) { // don't sent empty msg.
+    channel.push('shout', { // send the message to the server on "shout" channel
+      name: name.value,     // get value of "name" of person sending the message
+      message: msg.value    // get message text (value) from msg input field.
+    });
+    msg.value = '';         // reset the message input field for next message.
+  }
+});
